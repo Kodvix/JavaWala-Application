813b92b38074356252f16e5baa558f92b6b1be91
Deepanshu Parihar <131267316+DeepJavagit@users.noreply.github.com>
2025-08-11 12:54:59 +0530

    new updated

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // Ensures correct path for deployment
   server: {
-    host: true, // Or '0.0.0.0'
+    host: true, // Or '0.0.0.0' for network access
     port: 5173
   },
   plugins: [react()],
